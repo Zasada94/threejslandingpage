ae8a86c19815ba19e0f5a7b54bec8b91a3c5c21f
Zasada94 <pawelzasada2@wp.pl>
2022-10-17 15:47:21 +0200

    added fade out hover effect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import gsap from "gsap";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -70,7 +71,7 @@ for (let i = 0; i < array.length; i += 3) {
 
 const colors = [];
 for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-	colors.push(1, 0, 0);
+	colors.push(0, 0.19, 0.4);
 }
 
 planeMesh.geometry.setAttribute(
@@ -104,18 +105,49 @@ function animate() {
 		const { color } = intersects[0].object.geometry.attributes;
 
 		//vertice 1
-		color.setX(intersects[0].face.a, 0);
-		color.setY(intersects[0].face.a, 0);
+		color.setX(intersects[0].face.a, 0.1);
+		color.setY(intersects[0].face.a, 0.5);
 		color.setZ(intersects[0].face.a, 1);
 		//vertice 2
-		color.setX(intersects[0].face.b, 0);
-		color.setY(intersects[0].face.b, 0);
+		color.setX(intersects[0].face.b, 0.1);
+		color.setY(intersects[0].face.b, 0.5);
 		color.setZ(intersects[0].face.b, 1);
 		//vertice 3
-		color.setX(intersects[0].face.c, 0);
-		color.setY(intersects[0].face.c, 0);
+		color.setX(intersects[0].face.c, 0.1);
+		color.setY(intersects[0].face.c, 0.5);
 		color.setZ(intersects[0].face.c, 1);
+
 		intersects[0].object.geometry.attributes.color.needsUpdate = true;
+
+		const initialColor = {
+			r: 0,
+			g: 0.19,
+			b: 0.4,
+		};
+		const hoverColor = {
+			r: 0.1,
+			g: 0.5,
+			b: 1,
+		};
+		gsap.to(hoverColor, {
+			r: initialColor.r,
+			g: initialColor.g,
+			b: initialColor.b,
+			onUpdate: () => {
+				//vertice 1
+				color.setX(intersects[0].face.a, hoverColor.r);
+				color.setY(intersects[0].face.a, hoverColor.g);
+				color.setZ(intersects[0].face.a, hoverColor.b);
+				//vertice 2
+				color.setX(intersects[0].face.b, hoverColor.r);
+				color.setY(intersects[0].face.b, hoverColor.g);
+				color.setZ(intersects[0].face.b, hoverColor.b);
+				//vertice 3
+				color.setX(intersects[0].face.c, hoverColor.r);
+				color.setY(intersects[0].face.c, hoverColor.g);
+				color.setZ(intersects[0].face.c, hoverColor.b);
+			},
+		});
 	}
 }
 
