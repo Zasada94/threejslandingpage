a66e6c756e9960e92c3eeee32e98658525c7c144
Zasada94 <pawelzasada2@wp.pl>
2022-10-17 14:12:38 +0200

    modifying with dat.gui

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import * as dat from "dat.gui";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -7,8 +8,40 @@ const camera = new THREE.PerspectiveCamera(
 	0.1,
 	1000
 );
-
 const renderer = new THREE.WebGLRenderer();
+const gui = new dat.GUI();
+const world = {
+	plane: {
+		width: 5,
+		height: 5,
+		widthSegments: 10,
+		heightSegments: 10,
+	},
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 50).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+	planeMesh.geometry.dispose();
+	planeMesh.geometry = new THREE.PlaneGeometry(
+		world.plane.width,
+		world.plane.height,
+		world.plane.widthSegments,
+		world.plane.heightSegments
+	);
+
+	const { array } = planeMesh.geometry.attributes.position;
+	for (let i = 0; i < array.length; i += 3) {
+		const x = array[i];
+		const y = array[i + 1];
+		const z = array[i + 2];
+
+		array[i + 2] = z + Math.random();
+	}
+}
 
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
@@ -22,8 +55,6 @@ const planeMaterial = new THREE.MeshPhongMaterial({
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
-
-console.log(planeMesh.geometry.attributes.position.array);
 
 const { array } = planeMesh.geometry.attributes.position;
 
